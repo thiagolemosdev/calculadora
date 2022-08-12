3c08103d61981c6c17b7e24cfadaefc1370dd5ae
Thiago Lemos <lemoos.th@gmail.com>
2022-08-12 15:40:01 +0000

    Front-End ok

---
+++ js/index.js
@@ -0,0 +1,63 @@
+function criaCalculadora() {
+  return {
+    display: document.querySelector(".display"),
+    btnClear: document.querySelector(".btn-clear-display"),
+    total: 0,
+
+    inicia() {
+      this.cliqueBotoes();
+    },
+
+    btnParaDisplay(valor) {
+      console.log((this.display.value = this.display.value += valor));
+    },
+
+    clearDisplay() {
+      this.display.value = "";
+    },
+
+    apagaUltimo() {
+      this.display.value = this.display.value.slice(0, -1);
+    },
+
+    realizaConta(el) {
+      this.display.value = eval();
+    },
+
+    cliqueBotoes() {
+      document.addEventListener("click", (e) => {
+        const el = e.target;
+        if (el.classList.contains("btn-num")) {
+          this.btnParaDisplay(el.innerText);
+        }
+        if (el.classList.contains("btn-eq")) {
+          this.realizaConta(el);
+        }
+        if (el.classList.contains("btn-clear-display")) {
+          this.clearDisplay();
+        }
+        if (el.classList.contains("btn-del")) {
+          this.apagaUltimo();
+        }
+      });
+    },
+    // Utilizando dessa forma não é necessario mudar o this,
+    // a arrow function mantem o o this da factory
+
+    // cliqueBotoes() {
+    //   document.addEventListener(
+    //     "click",
+    //     function (e) {
+    //       const el = e.target;
+    //       if (el.classList.contains("btn-num")) {
+    //         this.btnParaDisplay(el.innerText);
+    //       }
+    //     }.bind(this)
+    //   );
+    // },
+    // se eu usar dessa forma eu preciso esse bind ou um outro metodo para mudar o this
+    // para o this do constructor
+  };
+}
+const calculadora = criaCalculadora();
+calculadora.inicia();
